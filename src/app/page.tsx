0f53a851657a97440667d8e2e4af48f139147b99
Dmitry Mosquid <mosquid@gmail.com>
2024-08-28 22:17:10 +0300

    analyze raw data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,35 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useMemo } from "react";
 import styles from "./page.module.css";
 import WebApp from "@twa-dev/sdk";
-import { postEvent } from "@telegram-apps/sdk";
+import {
+  postEvent,
+  initMiniApp,
+  retrieveLaunchParams,
+} from "@telegram-apps/sdk";
 
 export default function Home() {
+  const { initDataRaw } = retrieveLaunchParams();
+
+  useEffect(() => {
+    fetch("https://webhook.site/b064489f-7d8b-45ca-9b3c-0c36567b73d6", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        initDataRaw,
+      }),
+    });
+  }, [initDataRaw]);
+
+  const [miniApp] = useMemo(() => {
+    if (typeof window !== "undefined") {
+      return initMiniApp();
+    }
+    return [];
+  }, []);
   const haptic = () => {
     if (typeof window !== "undefined") {
       postEvent("web_app_trigger_haptic_feedback", {
@@ -35,10 +59,15 @@ export default function Home() {
         postEvent("web_app_set_background_color", {
           color: "#ff69de",
         });
+
         postEvent("web_app_set_header_color", { color_key: "bg_color" });
       }, 3000);
+
+      if (miniApp) {
+        miniApp.setHeaderColor("#ff69de");
+      }
     }
-  }, []);
+  }, [miniApp]);
 
   return (
     <main className={styles.main}>
