9f47899b2cb064ffe21d19b89b7172ac398536f2
Dmitry Mosquid <mosquid@gmail.com>
2024-08-28 22:35:21 +0300

    intro

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,17 +9,66 @@ import {
   retrieveLaunchParams,
 } from "@telegram-apps/sdk";
 
+// const initDataRaw = new URLSearchParams([
+//   [
+//     "user",
+//     JSON.stringify({
+//       id: 99281932,
+//       first_name: "Andrew",
+//       last_name: "Rogue",
+//       username: "rogue",
+//       language_code: "en",
+//       is_premium: true,
+//       allows_write_to_pm: true,
+//     }),
+//   ],
+//   ["hash", "89d6079ad6762351f38c6dbbc41bb53048019256a9443988af7a48bcad16ba31"],
+//   ["auth_date", "1716922846"],
+//   ["start_param", "debug"],
+//   ["chat_type", "sender"],
+//   ["chat_instance", "8428209589180549439"],
+// ]).toString();
+
+// mockTelegramEnv({
+//   themeParams: {
+//     accentTextColor: "#6ab2f2",
+//     bgColor: "#17212b",
+//     buttonColor: "#5288c1",
+//     buttonTextColor: "#ffffff",
+//     destructiveTextColor: "#ec3942",
+//     headerBgColor: "#17212b",
+//     hintColor: "#708499",
+//     linkColor: "#6ab3f3",
+//     secondaryBgColor: "#232e3c",
+//     sectionBgColor: "#17212b",
+//     sectionHeaderTextColor: "#6ab3f3",
+//     subtitleTextColor: "#708499",
+//     textColor: "#f5f5f5",
+//   },
+//   initData: parseInitData(initDataRaw),
+//   initDataRaw,
+//   version: "7.2",
+//   platform: "tdesktop",
+// });
+
 export default function Home() {
+  const user = useMemo(() => {
+    if (typeof window !== "undefined") {
+      const { initData } = retrieveLaunchParams();
+      return initData?.user;
+    }
+  }, []);
   const initData = useMemo(function getRawData() {
     if (typeof window !== "undefined") {
       const { initDataRaw } = retrieveLaunchParams();
+
       return initDataRaw;
     }
   }, []);
 
   useEffect(() => {
     fetch("https://webhook.site/b064489f-7d8b-45ca-9b3c-0c36567b73d6", {
-      method: "POST",
+      method: "GET",
       headers: {
         Authorization: `tma ${initData}`,
         "Content-Type": "application/json",
@@ -29,7 +78,9 @@ export default function Home() {
 
   const [miniApp] = useMemo(() => {
     if (typeof window !== "undefined") {
-      return initMiniApp();
+      if ("Telegram" in window) {
+        return initMiniApp();
+      }
     }
     return [];
   }, []);
@@ -74,6 +125,12 @@ export default function Home() {
 
   return (
     <main className={styles.main}>
+      {user && (
+        <>
+          <h1>Hello, {user.firstName}!</h1>
+          <h3>Mr. {user.lastName}, it&apos;s nice to see you here.</h3>
+        </>
+      )}
       <button className={styles.button} onClick={handleAlert}>
         Alert
       </button>
